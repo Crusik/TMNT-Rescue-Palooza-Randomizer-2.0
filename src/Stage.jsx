57a854cd30126bb511a491890c8d6809c71e4a91
Shawn Krietemeyer <shawn.krietemeyer@hotmail.com>
2023-06-22 21:03:36 -0500

    Got the stages sorted out somewhat properly. Added ids to stage/character divs

--- src/Stage.jsx
+++ src/Stage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Stage.css';
 
-function Stage(){
-    return (
-        <div className="stage">
-                <h2 id="0">Stage</h2>
-                <ul>
-                    <li><span id="13" className="character"></span></li>
-                    <li><span id="14" className="character"></span></li>
-                </ul>
-            </div>
-    );
+function Stage(props) {
+  const { stageName, player1Id, player2Id, stageId } = props;
+
+  return (
+    <div className="stage">
+      <h2 id={stageId}>{stageName || 'Stage'}</h2>
+      <ul>
+        <li><span id={player1Id} className="character"></span></li>
+        <li><span id={player2Id} className="character"></span></li>
+      </ul>
+    </div>
+  );
 }
+
 // function Stage(){
 //     return (
 //         <div className="stage">
