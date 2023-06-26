64cf11ceb61b2b114d1d70c81af164684c41910d
Shawn Krietemeyer <shawn.krietemeyer@hotmail.com>
2023-06-25 21:16:40 -0500

    Added Redux and started building the data

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const isActive = () => {
+    return {
+        type: "ISACTIVE"
+    };
+};
+
+export const isInactive = () => {
+    return {
+        type: "ISINACTIVE"
+    };
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,1 @@
+// All reducers will be imported here
