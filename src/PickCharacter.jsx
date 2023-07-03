c247454a27a7d5fd9fdb8af6ec140dc2cee4fb12
Shawn Krietemeyer <shawn.krietemeyer@hotmail.com>
2023-07-02 21:27:05 -0500

    Fixed the character being allowed to be selected for player 1 and player 2 for the same stage.

--- src/PickCharacter.jsx
+++ src/PickCharacter.jsx
@@ -15,36 +15,42 @@ const PickCharacter = () => {
   const characterButtonStatus = useSelector((state) => state.buttonStatus.characterButton);
   const stageIndex = useSelector((state) => state.stageIndex.stageIndexCount);
 
-  const activeCharacters = stageListData.flatMap((stage) =>
+  let activeCharacters = stageListData.flatMap((stage) =>
     stage.characters.filter((character) => character.isActive).map((character) => character.character)
   );
 
   const runTwice = (func) => {
-    func();
-    setTimeout(() => {
-      func();
+    func(activeCharacters, (selectedCharacter) => {  
+      console.log(selectedCharacter.character)
       setTimeout(() => {
-        if(stageIndex < 13){
-          dispatch(enableButton({ buttonId: 'stageButton' }));
-          // This seems wrong, but it works because characterIndex is updated, but cannot be accessed until this function completes.
-        } else if (characterIndex < 45) {
-          dispatch(enableButton({ buttonId: 'characterButton' }));
-        } else {
-          // Run Cowabunga(). This might need to be changed to work properly.
-        }
-      }, 10000);
-    }, 10000);
+        const filteredCharacters = activeCharacters.filter(character => character !== selectedCharacter.character);
+        console.log(filteredCharacters)
+        func(filteredCharacters);
+        setTimeout(() => {
+          if(stageIndex < 13){
+            dispatch(enableButton({ buttonId: 'stageButton' }));
+            // This seems wrong, but it works because characterIndex is updated, but cannot be accessed until this function completes.
+          } else if (characterIndex < 45) {
+            dispatch(enableButton({ buttonId: 'characterButton' }));
+          } else {
+            // Run Cowabunga(). This might need to be changed to work properly.
+          }
+        }, 10000);
+        console.log(selectedCharacter.character)
+      }, 1000);
+    });
   };
 
   const handleRunTwice = () => {
     runTwice(handleCharacterSelection);
   };
 
-  const handleCharacterSelection = () => {
+  const handleCharacterSelection = (activeCharactersList, callback) => {
+    console.log(activeCharactersList)
     const characterIndex = characterIndexRef.current;
     console.log(characterIndex);
     const minTimer = 10;
-    const maxTimer = 18;
+    const maxTimer = 19;
     let headerCharacters = document.getElementById('headerCharacters');
     randomStageSound();
     dispatch(disableBothButtons());
@@ -52,37 +58,36 @@ const PickCharacter = () => {
     function characterTimer(min, max) {
       let i = 0;
       let intervalHandle = setInterval(function () {
-        headerCharacters.textContent = activeCharacters[i++ % activeCharacters.length];
+        headerCharacters.textContent = activeCharactersList[i++ % activeCharactersList.length];
       }, 60);
       var rand = Math.floor(Math.random() * (max - min + 1) + min);
       console.log(rand / 2 + ' seconds');
-      setTimeout(function () {
+      setTimeout(function() {
         clearInterval(intervalHandle);
       }, 500 * rand);
       function displayCharacter() {
         setTimeout(() => {
-          let characters = document.getElementById(characterIndex);
-          characters.textContent = headerCharacters.textContent;
-          const tl = gsap.timeline({ defaults: { ease: 'power0.out' } });
-          tl.to(characters, { autoAlpha: '1', duration: 1 });
-          tl.to(characters, { transform: 'translateX(0)', duration: 1 }, '-=1');
-          const selectedCharacter = stageListData.flatMap(stage => stage.characters)
-          .find(character => character.character === headerCharacters.textContent);
+            let characters = document.getElementById(characterIndex);
+            characters.textContent = headerCharacters.textContent;
+            const tl = gsap.timeline({ defaults: { ease: 'power0.out' } });
+            tl.to(characters, { autoAlpha: '1', duration: 1 });
+            tl.to(characters, { transform: 'translateX(0)', duration: 1 }, '-=1');
+            const selectedCharacter = stageListData.flatMap(stage => stage.characters)
+            .find(character => character.character === headerCharacters.textContent);
 
-        if (selectedCharacter) {
-          // Dispatch an action to increment the count of the selected character
-          dispatch(incrementCount(selectedCharacter.character));
-          // Dispatch an action to deactivate the character
-          dispatch(deactivateCharacter(selectedCharacter.character));
-        if (characterIndex < 47) {
-          dispatch(incrementCharacterIndex());
-          // The following is to add to a reference of characterIndex that is local to this function. Since this function is ran twice in a row, the characterIndex is updated, but
-          // the function cannot access it while the runTwice() function is still running on the second func() call.
-          characterIndexRef.current += 1;
-          }
-        }
-      }, 500 * rand + 50);
-    }
+            if (selectedCharacter) {
+              dispatch(incrementCount(selectedCharacter.character));
+              dispatch(deactivateCharacter(selectedCharacter.character));
+              if (characterIndex < 47) {
+                dispatch(incrementCharacterIndex());
+                // The following is to add to a reference of characterIndex that is local to this function. Since this function is ran twice in a row, the characterIndex is updated, but
+                // the function cannot access it while the runTwice() function is still running on the second func() call.
+                characterIndexRef.current += 1;
+              }
+            }
+            return callback(selectedCharacter);
+          }, 500 * rand + 50);
+      }
     displayCharacter();
   }
 
