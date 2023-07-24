b81f4ad32293adee123ff97dd920eadc19f205df
Crusik <crusik@hotmail.com>
2023-07-23 21:04:56 -0500

    Added some images and reversed changes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,7 +5,7 @@ import PickStage from './PickStage';
 import RemoveStage from './RemoveStage';
 import RemoveCharacter from './RemoveCharacter';
 
-
+// I want to Add selector for Normal and "Arcade" modes. For Arcade mode, all of the characters will be unlocked. Even the extra characters.
 
 const Sidebar = ({ handleShowImage }) => {
     return (
